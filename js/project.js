92d6c43f86b0b073d64f347a58181f126297def2
prashantmore277 <prashantmore277@gmail.com>
2022-01-01 19:09:25 +0530

    Added TimeINText and BorderColorChange

--- js/project.js
+++ js/project.js
@@ -1,16 +1,34 @@
 setInterval(setClock, 1000)
-
-const secondHand = document.querySelector('[data-second-hand]')
-
-const minuteHand = document.querySelector('[data-minute-hand]')
-
+setInterval(getTimeInText, 1000)
 const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+const time = document.querySelector('[time-in-text]')
+const colorArray = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green',
+    'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red',
+    'silver', 'teal', 'white', 'yellow'
+];
+
+
+function ChangeBoderColor() {
+    let colorIndex = Math.floor((Math.random() * 15) + 1);
+    document.getElementById("time").style.borderColor = colorArray[colorIndex];
+    // document.getElementById('time').innerHTML = colorIndex;
+}
+
+function getTimeInText() {
+    const d = new Date();
+    let time = d.getHours() % 12 + " : " + d.getMinutes() + " : " + d.getSeconds();
+    document.getElementById("time").innerHTML = time;
+    ChangeBoderColor()
+}
 
 function setClock() {
     const currentDate = new Date()
+
     const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = (secondsRatio + currentDate.getMinutes() / 60)
-    const hoursRatio = (minutesRatio + currentDate.getHours() / 12)
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
 
     setRotation(secondHand, secondsRatio)
     setRotation(minuteHand, minutesRatio)
@@ -24,3 +42,4 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
+getTimeInText()
